bdb78c9bbd0f2cb867b50c8d1ce6f2f5c82076c9
yezhiyi9670 <39194383+yezhiyi9670@users.noreply.github.com>
2020-07-10 13:24:20 +0800

    I don't know what I did

--- src/main/java/yezhiyi9670/calmparticles/mixins/MixinParticleManager.java
+++ src/main/java/yezhiyi9670/calmparticles/mixins/MixinParticleManager.java
@@ -16,6 +16,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 import net.minecraft.client.particle.ItemPickupParticle;
+import net.minecraft.client.renderer.entity.ItemRenderer;
 
 import java.util.Map;
 import java.util.Queue;
